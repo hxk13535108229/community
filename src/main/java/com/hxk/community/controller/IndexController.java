f2fe94c8b958880aee4e3af576ea0d67871ac782
hxk <1429195907@qq.com>
2021-08-19 15:16:49 +0800

    init repo

---
+++ src/main/java/com/hxk/community/controller/IndexController.java
@@ -0,0 +1,19 @@
+package com.hxk.community.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @ClassName IndexController
+ * @Description TODO
+ * @Author OvO
+ * @Date 2021-08-19 15:09
+ * @Version 1.0
+ **/
+@Controller
+public class IndexController {
+    @RequestMapping("/")
+    public String goIndex(){
+        return "index";
+    }
+}
