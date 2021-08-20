a793b9d8a81a1cac967f9defbc654529369e4ee8
hxk <1429195907@qq.com>
2021-08-20 10:22:02 +0800

    add lombok

--- src/main/java/com/hxk/community/model/Question.java
+++ src/main/java/com/hxk/community/model/Question.java
@@ -1,5 +1,7 @@
 package com.hxk.community.model;
 
+import lombok.Data;
+
 /**
  * @ClassName Question
  * @Description TODO
@@ -7,96 +9,16 @@ package com.hxk.community.model;
  * @Date 2021-08-20 1:56
  * @Version 1.0
  **/
+@Data
 public class Question {
     private Integer id;
-    private String title;
-    private String description;
-    private String tag;
-    private Long gmt_create;
-    private Long gmt_modify;
-    private Integer creator;
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public String getTag() {
-        return tag;
-    }
-
-    public void setTag(String tag) {
-        this.tag = tag;
-    }
-
-    public Long getGmt_create() {
-        return gmt_create;
-    }
-
-    public void setGmt_create(Long gmt_create) {
-        this.gmt_create = gmt_create;
-    }
-
-    public Long getGmt_modify() {
-        return gmt_modify;
-    }
-
-    public void setGmt_modify(Long gmt_modify) {
-        this.gmt_modify = gmt_modify;
-    }
-
-    public Integer getCreator() {
-        return creator;
-    }
-
-    public void setCreator(Integer creator) {
-        this.creator = creator;
-    }
-
-    public Integer getComment_count() {
-        return comment_count;
-    }
-
-    public void setComment_count(Integer comment_count) {
-        this.comment_count = comment_count;
-    }
-
-    public Integer getView_count() {
-        return view_count;
-    }
-
-    public void setView_count(Integer view_count) {
-        this.view_count = view_count;
-    }
-
-    public Integer getLike_count() {
-        return like_count;
-    }
-
-    public void setLike_count(Integer like_count) {
-        this.like_count = like_count;
-    }
-
-    private Integer comment_count;
-    private Integer view_count;
-    private Integer like_count;
+    private String title;           //标题
+    private String description;     //描述
+    private String tag;             //标签
+    private Long gmt_create;        //创建时间
+    private Long gmt_modify;        //修改时间
+    private Integer creator;        //创建者id
+    private Integer comment_count;  //评论数
+    private Integer view_count;     //观看数
+    private Integer like_count;     //点赞数
 }
--- src/main/java/com/hxk/community/model/User.java
+++ src/main/java/com/hxk/community/model/User.java
@@ -1,5 +1,7 @@
 package com.hxk.community.model;
 
+import lombok.Data;
+
 /**
  * @ClassName User
  * @Description TODO
@@ -7,60 +9,12 @@ package com.hxk.community.model;
  * @Date 2021-08-19 23:41
  * @Version 1.0
  **/
+@Data
 public class User {
-    private String account_id;
-    private String account_name;
-    private String token;
-    private Long gmt_create;
-
-    public String getAccount_id() {
-        return account_id;
-    }
-
-    public void setAccount_id(String account_id) {
-        this.account_id = account_id;
-    }
-
-    public String getAccount_name() {
-        return account_name;
-    }
-
-    public void setAccount_name(String account_name) {
-        this.account_name = account_name;
-    }
-
-    public String getToken() {
-        return token;
-    }
-
-    public void setToken(String token) {
-        this.token = token;
-    }
-
-    public Long getGmt_create() {
-        return gmt_create;
-    }
-
-    public void setGmt_create(Long gmt_create) {
-        this.gmt_create = gmt_create;
-    }
-
-    public Long getGmt_modify() {
-        return gmt_modify;
-    }
-
-    public void setGmt_modify(Long gmt_modify) {
-        this.gmt_modify = gmt_modify;
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    private Long gmt_modify;
+    private String account_id;      //用户id
+    private String account_name;    //用户名字
+    private String token;           //accesstoken
+    private Long gmt_create;        //创建时间
+    private Long gmt_modify;        //修改时间
     private Integer id;
 }
